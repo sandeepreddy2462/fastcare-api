37982efe202b3676f99a8c87409582ba538187b6
Sandeep Reddy Bhumireddy <157559130+sandeepreddy2462@users.noreply.github.com>
2025-08-14 18:16:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,6 @@ async def result(
         raise HTTPException(status_code=500, detail=f"Error processing request: {str(e)}")
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
